2baa1a85b02e2f328313a90da34d9956531cb3ad
Anum Qudsia <hello@anum.io>
2021-05-03 02:55:03 +0200

    Fix font and icons. Add back and forth arrows.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,49 @@
-import React, { useMemo, useState } from "react";
-import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { DisplayPdf, MyDocument } from "./Components/PDF/DisplayPdf";
 import { PDFData } from "./Components/PDF/PDFData";
-import {
-  PDFDownloadLink,
-  Document,
-  Page,
-  View,
-  Text,
-  BlobProvider,
-} from "@react-pdf/renderer";
+import { PDFDownloadLink } from "@react-pdf/renderer";
 import { Button } from "@material-ui/core";
+import styled from "styled-components";
+import WebFont from "webfontloader";
 
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+
+const Container = styled.div`
+  background: lightgray;
+  font-family: "Raleway";
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  background: #efefef;
+  margin: 2px;
+  width: 2rem;
+  justify-content: center;
+  border-radius: 2px;
+
+  :hover {
+    background: lightgray;
+  }
+`;
 function App() {
-  const [currentState, setCurrentState] = useState(1);
+  const [currentState, setCurrentState] = useState(0);
 
   const docTitle = "iSNITT Timeliste April 2021";
+
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: [
+          "Droid Sans",
+          "Chilanka",
+          "Open Sans",
+          "Raleway",
+          "Quicksand",
+        ],
+      },
+    });
+  }, []);
 
   const MyDoc = () => {
     return <MyDocument title={docTitle} />;
@@ -46,8 +73,42 @@ function App() {
     );
   };
 
+  const buttonStyle = {
+    minWidth: "inherit",
+    color: "#484848",
+  };
+
   const SwitchComps = () => {
     switch (currentState) {
+      case 0:
+        return (
+          <Container>
+            <h4>Timesheet</h4>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              {/* back and front arrow */}
+              <ButtonContainer>
+                <Button
+                  style={buttonStyle}
+                  onClick={() => {
+                    console.log("left");
+                  }}
+                >
+                  <FaChevronLeft />
+                </Button>
+              </ButtonContainer>
+              <ButtonContainer>
+                <Button
+                  style={buttonStyle}
+                  onClick={() => {
+                    console.log("right");
+                  }}
+                >
+                  <FaChevronRight />
+                </Button>
+              </ButtonContainer>
+            </div>
+          </Container>
+        );
       case 1:
         return (
           <>
